8710242645318151112398862b6b4f2dc29bdd1d
Glitch (hello-express) <none>
2020-02-19 20:38:36 +0000

    🚲💸 Checkpoint ./server.js:4998155/2253

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ client.on('ready', () => {
 
 
 const GUILDID = '605544586768547850'; // اي دي السيرفر  
-const CHANNELID = '654468730121879592'; // اي دي الروم
+const CHANNELID = '659191097817301023'; // اي دي الروم
 const ownerID = ["516473846983950336"]; // ايدي ادارة البوت او صاحب البوت ..
 
 
@@ -72,27 +72,7 @@ let cmds = {
   nowplaying: { cmd: 'Nowplaying', a: ['np'] }
 };
 
-client.on('ready',async () => {
-  setInterval(() => {
-client.channels.find(ch => ch.id === CHANNELID && ch.type === 'voice').join();
-},1000); 
- voiceStay(GUILDID, CHANNELID);
-  function voiceStay(guildid, channelid) {
-    if(!guildid) throw new Error('Syntax: voiceStay function requires guildid');
-    if(!channelid) throw new Error('Syntax: voiceStay function requires channelid');
 
-    let guild = client.guilds.get(guildid);
-    let channel = guild.channels.get(channelid);
-
-    if(channel.type === 'voice') {
-      channel.join().catch(e => {
-        console.log(`Failed To Join :: ${e.message}`);
-      });
-    } else {
-      console.log(`Channel Type ::  ${channel.type}, It must be Voice.`);
-    }
-  }
-});
 
 
 Object.keys(cmds).forEach(key => {
